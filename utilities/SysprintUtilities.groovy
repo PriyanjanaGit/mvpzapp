4a3da392aafa6e0aff9771455bed1cc278ddb5b6
PriyanjanaGit <111520535+PriyanjanaGit@users.noreply.github.com>
2023-05-10 17:18:47 +0100

    Create SysprintUtilities.groovy

---
+++ utilities/SysprintUtilities.groovy
@@ -0,0 +1,25 @@
+@groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
+import com.ibm.dbb.build.*
+import groovy.transform.*	
+// sysPrint Method
+def sysPrint(String prnPDS, String member, File logFile) {	
+	new CopyToPDS().file(new File("$logFile"))
+    	.hfsEncoding("UTF-8")
+    	.dataset(prnPDS)
+    	.pdsEncoding("IBM-1047")
+    	.member(member)
+    	.execute()
+   println("***** Ray Lam sysPrint copyToPADS finished")
+   String printPDS = prnPDS + "($member)"
+   return printPDS		
+}
+// headLines Method
+def headLines(File logFile, String headLine, String headersPDS) {
+    new CopyToHFS().file(new File("$logFile"))
+    .hfsEncoding("UTF-8")
+    .dataset("$headersPDS")
+    .member("$headLine")
+    .append(true)
+    .execute()		
+     return
+}
